5269ecda29f101b8416281411d4a816c46a174b2
turbogag1 <gabrielramos16969@gmail.com>
2025-10-20 20:43:47 +0800

    Created AdminPost, Fixtures, validators, contraints, etc.

--- pages/admin.py
+++ pages/admin.py
@@ -1,8 +1,25 @@
 from django.contrib import admin
 from pages.models import Post, Comment, Student, Course, Enrollment
 # Register your models here.
-admin.site.register(Post)
-admin.site.register(Comment)
-admin.site.register(Student)
-admin.site.register(Course)
-admin.site.register(Enrollment)
+class CommentInLine(admin.StackedInline):
+    model = Comment
+    extra = 1
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "body", "created_at")
+    search_fields = ("title", "body")
+    list_filter = ("created_at",)
+    ordering = ("-created_at",)
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("author", "post", "created_at")
+    search_fields = ("author", "text")
+    list_filter = ("created_at",)
+
+#@admin.register(Student)
+
+#@admin.register(Course)
+
+#@admin.register(Enrollment)
--- pages/models.py
+++ pages/models.py
@@ -1,16 +1,20 @@
+from django.core.validators import MinLengthValidator
 from django.db import models
 
 # Create your models here.
 class Post(models.Model):
-    title = models.CharField(max_length=100)
-    body = models.TextField()
+    title = models.CharField(max_length=100, validators=[MinLengthValidator(3)])
+    body = models.TextField(validators=[MinLengthValidator(10)])
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         ordering = ['-created_at']
+        constraints = [
+        models.UniqueConstraint(fields=['title'], name='unique_post_title')
+        ]
     
     def __str__(self):
-        return self.title
+       return self.title
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
